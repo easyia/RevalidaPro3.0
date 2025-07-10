9b823c7b1570767d3b2e62d467bfcb729663ac30
easyia <admin@easyai.com>
2025-07-10 14:07:47 -0300

    Corrige conflitos e restaura build do Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> edfd5a9274d9a96c3e18b5281c89f2e90dc5f0bc
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -14,20 +13,3 @@ export default defineConfig({
   // Nenhuma configuração de build customizada.
   // Deixaremos o Vite e o Vercel usarem os padrões otimizados.
 });
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  // Nenhuma configuração de build customizada.
-  // Deixaremos o Vite e o Vercel usarem os padrões otimizados.
-});
->>>>>>> edfd5a9274d9a96c3e18b5281c89f2e90dc5f0bc
